4a65fbc66664ea7eb4a3d81c5ed3a8bc83cb47ee
Serinolli <rafael.serinolli2@gmail.com>
2020-05-28 15:22:03 -0300

    Finalizado
    Projeto para a aula de 28/05 concluído, 5 marcadores estáticos adicionados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,25 +13,35 @@ declare var google: any;
 
 export class HomePage {
 
-    map: any;
-    posicaoAtual: any;
+  map: any;
+  posicaoAtual: any;
+  marker: any;
+  marker2: any;
+  marker3: any;
+  marker4: any;
+  marker5: any;
 
-    //'map' declarado no html, pela # 
-    //criada uma div de referência, irá apontar para o que for criado na variável "google"
-    @ViewChild('map', {read: ElementRef, static:false}) mapRef:ElementRef;
+  
+  //'map' declarado no html, pela # 
+  //criada uma div de referência, irá apontar para o que for criado na variável "google"
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
 
-  constructor(private geolocation: Geolocation) {}
 
-  public async showMap(){
-    //const location = new google.maps.LatLng(-22.4807481, -48.5633106); (posição fixa)
-    await this.buscaPosicao();
 
+  constructor(private geolocation: Geolocation) { }
+
+  public async showMap() {
+   // const location1 = new google.maps.LatLng(-22.4807481, -48.5633106); 
+        await this.buscaPosicao();
+       
+        
     const options = {
       //location é a variável criada acima
       center: this.posicaoAtual,
-      zoom: 15,
+      zoom: 12,
       disableDefaultUI: true
     }
+
     //criação do mapa (apenas criado, lembrar de "colocar para aparecer", ou seja, chama-lo)
     this.map = new google.maps.Map(this.mapRef.nativeElement, options)
 
@@ -41,17 +51,57 @@ export class HomePage {
       map: this.map,
       title: "Localização atual",
       //colocar ícones
-      icon:'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-      animation: google.maps.Animation.BOUNCE
-    })
+      icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+      animation: google.maps.Animation.BOUNCE})
+
+      const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(-22.4807481, -48.5633106),
+        map: this.map,
+        title: "Barra Bonita, cidade simpatia!",
+        //colocar ícones
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        animation: google.maps.Animation.DROP})
+
+        const marker2 = new google.maps.Marker({
+          position: new google.maps.LatLng(-22.5122094, -48.5575188),
+          map: this.map,
+          title: "Perigoso aqui em!",
+          //colocar ícones
+          icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+          animation: google.maps.Animation.DROP})
+
+        const marker3 = new google.maps.Marker({
+          position: new google.maps.LatLng(-22.5133455, -48.7329254),
+          map: this.map,
+          title: "Tem nada aqui",
+          //colocar ícones
+          icon: 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+          animation: google.maps.Animation.DROP})
+
+          const marker4 = new google.maps.Marker({
+            position: new google.maps.LatLng(-22.608839, -48.791712),
+            map: this.map,
+            title: "LPNET é muito bom",
+            //colocar ícones
+            icon: 'https://maps.google.com/mapfiles/ms/icons/pink-dot.png',
+            animation: google.maps.Animation.DROP})
+
+            const marker5 = new google.maps.Marker({
+              position: new google.maps.LatLng(-22.675501, -48.668346),
+              map: this.map,
+              title: "Será que tem muita areia aqui?",
+              //colocar ícones
+              icon: 'https://maps.google.com/mapfiles/ms/icons/orange-dot.png',
+              animation: google.maps.Animation.DROP})
+  
   }
   //"ionViewDidEnter" quando a página for aparecer, o mapa será chamado, ou seja, o mapa carrega APÓS a página.
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     this.showMap();
   }
 
   //o codigo para continuar recebendo a localização do usuário não pdoe ser posto solto, deve-se criar uma estrutura como:
-  public async buscaPosicao(){
+  public async buscaPosicao() {
     await this.geolocation.getCurrentPosition().then((posicaoGPS) => {
       this.posicaoAtual = {
         lat: posicaoGPS.coords.latitude,
@@ -59,9 +109,11 @@ export class HomePage {
       }
       // resp.coords.latitude
       // resp.coords.longitude
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });    
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+    
   }
+  
 
 }
